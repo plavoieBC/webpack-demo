3f95ba5f3c723a0b96b618e9294f02022c56356b
plavoieBC <patrick.lavoie@gov.bc.ca>
2016-08-22 12:11:23 -0700

    DefinePlugin setup

--- libs/parts.js
+++ libs/parts.js
@@ -33,3 +33,26 @@ exports.setupCSS = function(paths) {
         }
     };
 }
+
+exports.minify = function() {
+    return {
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            })
+        ]
+    };
+}
+
+exports.setFreeVariable = function(key, value) {
+    const env = {};
+    env[key] = JSON.stringify(value);
+
+    return {
+        plugins: [
+            new webpack.DefinePlugin(env)
+        ]
+    };
+}
